d9c92aab62bf9ee5146862139ac9a0d7f54303e6
mutugi001 <collinsmut04@gmail.com>
2023-01-04 19:33:15 +0000

    islower

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-
-
-    for i in range(97,123):
+    if i in range(97, 123):
         if c == chr(i):
             return(True)
         else:
             return(False)
+    else:
+        return(False)
