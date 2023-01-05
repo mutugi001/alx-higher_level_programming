93432ebfae9453742262129dc9970a8f3a56cb21
mutugi001 <collinsmut04@gmail.com>
2023-01-05 00:47:27 +0000

    remove char

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
+    if n > len(str):
+        copy = str
+    if str == '\0':
+        copy = '\0'
     for i in str:
         if i != str[n]:
             copy = str
