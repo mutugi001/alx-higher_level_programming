c485e015817558b43ebb262fdecdecb92901ccde
mutugi001 <collinsmut04@gmail.com>
2023-01-06 13:33:05 -0800

    maths

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, a + b))
+    
+
+def sub(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, a - b))
+    
+
+def mul(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, a * b))
+    
+
+def div(a, b):
+    a = 10
+    b = 5
+    print("{} + {} = {:.0f}".format(a, b, a / b)) 
+    
+add(10, 5)
+sub(10, 5)
+mul(10, 5)
+div(10, 5)
