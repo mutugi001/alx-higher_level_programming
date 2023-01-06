6d0f838ad66acf35f38e3954fde61f7cd153a0b4
mutugi001 <collinsmut04@gmail.com>
2023-01-06 13:14:39 -0800

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,2 @@
 #!/usr/bin/python3
-def add(a, b):
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, a + b))
-    return(a + b)
-
-add(1, 2)
+import add_0
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add(a, b):
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, a + b))
+
+add(1, 2)
+
