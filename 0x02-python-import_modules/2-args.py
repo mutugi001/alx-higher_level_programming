80a6fd9c8d84d8766782e8d4ba56c51fa343663f
mutugi001 <collinsmut04@gmail.com>
2023-01-06 15:32:56 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(argv - 1))
         for i in range(1, argv):
-            if argv == 1:
                 print("{}: {}".format(i, sys.argv[i]))
