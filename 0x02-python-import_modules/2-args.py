efa28aa0dfe3c3a8d18125684fe0a01b216924e1
mutugi001 <collinsmut04@gmail.com>
2023-01-06 15:27:21 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import sys
-argv = len(sys.argv)
-if argv == 0:
-    print("0 arguments.")
-if argv == 2:
-    print("{} arguement:".format(argv - 1))
-    print("1: {}".format(sys.argv[1]))
-else:
-    print("{} arguments:".format(argv - 1))
-    for i in range(1, argv):
-        if argv == 1:
-            print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    argv = len(sys.argv)
+    if argv == 0:
+        print("0 arguments.")
+    if argv == 2:
+        print("{} arguement:".format(argv - 1))
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(argv - 1))
+        for i in range(1, argv):
+            if argv == 1:
+                print("{}: {}".format(i, sys.argv[i]))
