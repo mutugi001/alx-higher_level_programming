468f097221db1d602c6dd8c91b648019c9600bc9
mutugi001 <collinsmut04@gmail.com>
2023-01-06 14:38:53 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,11 @@ import sys
 argv = len(sys.argv)
 if argv == 0:
     print("0 arguments.")
+if argv == 2:
+    print("{} arguement:".format(argv - 1))
+    print("1: {}".format(sys.argv[1]))
 else:
     print("{} arguments:".format(argv - 1))
     for i in range(1, argv):
-        print("{}: {}".format(i, sys.argv[i]))
+        if argv == 1:
+            print("{}: {}".format(i, sys.argv[i]))
