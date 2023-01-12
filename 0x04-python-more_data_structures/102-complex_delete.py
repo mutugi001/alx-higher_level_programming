9f79d49581e8ed6b8b7471a6be4bfd58c2df3a6c
mutugi001 <collinsmut04@gmail.com>
2023-01-11 19:16:36 -0800

    complex delete

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    if value is None:
+        return(a_dictionary)
+    for i, j in a_dictionary:
+        if j == value:
+            del(a_dictionary[i])
+    return(a_dictionary)
