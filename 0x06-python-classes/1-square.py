699b26edb3c4f14917251c501eda8bf5f0c3e656
mutugi001 <colinsmut04@gmail.com>
2023-01-26 02:49:48 -0800

    fix

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,12 +4,8 @@
 
 class Square:
     """represents a square"""
-
-
     def __init__(self, size):
         """initializing a new square"""
         """Args -
         size  parameter of size of sqaure"""
-
-
         self.__size = size
