4c0ad1a1c4612726ef3802ab48f92b5316a7d8ad
mutugi001 <colinsmut04@gmail.com>
2023-01-26 02:48:34 -0800

    square init

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
-import 0-square
-x = square()
-def __init__(self, size):
-    self.__size = size
-    x.
+"""defining a class square"""
+
+
+class Square:
+    """represents a square"""
+
+
+    def __init__(self, size):
+        """initializing a new square"""
+        """Args -
+        size  parameter of size of sqaure"""
+
+
+        self.__size = size
