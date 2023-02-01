4edbba11328de4f21236092d9dfa026de0e5fbed
mutugi001 <colinsmut04@gmail.com>
2023-02-01 06:10:28 -0800

    repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -70,4 +70,4 @@ class Rectangle:
 
     def __repr__(self):
         """function to print rectangle whe repr is called"""
-        return("Rectangle({}, {})".format(self.__width,self.__height))
+        return("Rectangle({}, {})".format(self.__width, self.__height))
