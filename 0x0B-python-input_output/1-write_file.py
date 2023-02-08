9616bb2b0b3858843d8b049f5adb7552dd765bf4
mutugi001 <collinsmut04@gmail.com>
2023-02-08 05:35:12 -0800

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    """function/method to write to a file and create if file does not exist """
     with open(filename, 'w', encoding='UTF8') as f:
         print(f.write(text))
