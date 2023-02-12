cbbc806bc4ceb9148b18b00f3c4f0f63e658162e
mutugi001 <collinsmut04@gmail.com>
2023-02-12 03:56:08 -0800

    fix

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,5 +4,8 @@
 
 def is_same_class(obj, a_class):
     """methof to check whether obj is an instance of a_class"""
-    return(isinstance(obj, a_class))
+    if type(obj) is a_class:
+        return (True)
+    else:
+        return (False)
 
