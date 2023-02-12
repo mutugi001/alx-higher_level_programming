d0cfd4f05e1d72574c6717dcae94637d525d7903
mutugi001 <collinsmut04@gmail.com>
2023-02-12 04:31:58 -0800

    inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def inherits_from(obj, a_class):
     """method to check if obj is an instance of a_class"""
-    if type(obj) ==  a_class:
+    if type(obj) == a_class:
         return(False)
     else:
         return(True)
