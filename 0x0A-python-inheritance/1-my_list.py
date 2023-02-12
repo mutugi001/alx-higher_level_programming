3769070d7f5370ca1c09a8443bf82ae534931ab2
mutugi001 <collinsmut04@gmail.com>
2023-02-12 01:55:30 -0800

    sort list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,5 +5,9 @@
 class MyList(list):
     def print_sorted(self):
         """returns a sorted list in ascending order"""
-
+        for i in self:
+            if type(i) is int:
+                pass
+            else:
+                raise TypeError("self should contain ints only")
         print(sorted(self))
