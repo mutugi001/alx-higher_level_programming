0b7f2a876615c107e5b2c9bae3c8bfe82a650774
mutugi001 <collinsmut04@gmail.com>
2023-02-12 04:52:06 -0800

    area method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""empty class creation"""
+
+
+class BaseGeometry:
+    def area(self):
+        """method area that takes no args and raises an
+        exception"""
+        raise Exception("area() is not implemented")
+
