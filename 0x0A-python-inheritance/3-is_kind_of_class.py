e292ebcf55d95c964eed84e63981bdeb79598cb0
mutugi001 <collinsmut04@gmail.com>
2023-02-12 03:49:16 -0800

    kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+##!/usr/bin/python3
+"""a method outside a class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns a value true or false depending on isinstance
+    of obj and class"""
+    return(isinstance(obj, a_class))
