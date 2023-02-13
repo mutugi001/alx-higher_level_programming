b987241ab6051783f4bf110e27025009c703fe82
mutugi001 <collinsmut04@gmail.com>
2023-02-13 11:14:18 -0800

    validate

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -8,6 +8,7 @@ class BaseGeometry:
         """method area that takes no args and raises an
         exception"""
         raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
         """method that validates value"""
         self.value = value
