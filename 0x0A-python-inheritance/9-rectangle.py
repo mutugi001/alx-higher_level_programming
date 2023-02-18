3daa2aeef32510c7aff594ceddbfcaecce364211
mutugi001 <collinsmut04@gmail.com>
2023-02-18 11:59:34 +0300

    Area

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""empty class creation"""
+
+
+class BaseGeometry:
+    """class created and attributes go here and methods"""
+
+    def __init__(self):
+        """initialization of class basegeometry"""
+        pass
+
+    def area(self, height, width):
+        """method area that takes no args and raises an
+        exception"""
+        # raise Exception("area() is not implemented")
+        self.height = height
+        self.width = width
+
+        area = self.width * self.height
+        # print("Area {}".format(area))
+        print("[Rectangle] {} {}".format(self.width, self.height))
+        return area
+
+    def integer_validator(self, name, value):
+        """method that validates value"""
+        self.name = name
+        self.value = value
+
+        if type(self.name) is not str:
+            raise TypeError("{} is not a string".format(self.name))
+        if type(self.value) is not int:
+            raise TypeError("{} must be an integer".format(self.name))
+        if self.value <= 0:
+            raise ValueError("{} must be greater than 0".format(self.name))
+
+
+"""class rectangle to create a rectangle"""
+
+
+class Rectangle(BaseGeometry):
+    """class rectangle created"""
+
+    def __init__(self, width, height):
+        """initialization of class rectangle with args:
+        width- width of rectangle
+        height - height of rectangle"""
+        self.__width = width
+        self.__height = height
+        BaseGeometry.__init__(self)
+        BaseGeometry.integer_validator(self, "width", self.__width)
+        BaseGeometry.integer_validator(self, "height", self.__height)
+
+        # wBaseGeometry.area(self, self.__height,self. __width)
+
+    def area(self):
+        # self.height = height
+        # self.width = width
+
+        area = self.__width * self.__height
+        print("[Rectangle] {} {}".format(self.__width, self.__height))
+        return str(area)
+        
